5b3e4de19d6e314f016c855789c03f5420d29d99
rishabhpundir <th3rishabh@gmail.com>
2023-05-12 16:49:19 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1 align="center">Hello, ReactJs CI/CD Pipeline Deployment through AWS is Finally Done!</h1>
+      <h1 align="center">Hello World!</h1>
     </div>
   );
 }
